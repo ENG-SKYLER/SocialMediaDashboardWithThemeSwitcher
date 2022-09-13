e2424a83a53a21a55ecde76ccb9482e1c51a40b1
ENG-SKYLER <ibraheema.a2014@gmail.com>
2022-09-13 18:21:28 +0200

    final coding

---
+++ script.js
@@ -0,0 +1,16 @@
+const colorSwitch = document.getElementById("input-color-switch");
+colorSwitch.addEventListener("click", checkMode);
+function checkMode() {
+  console.log("checking...");
+  if (colorSwitch.checked) {
+    darkModeOn();
+  } else {
+    darkModeOff();
+  }
+}
+function darkModeOn() {
+  document.body.classList.add("dark-mode");
+}
+function darkModeOff() {
+  document.body.classList.remove("dark-mode");
+}
